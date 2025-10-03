0d51f0660765e4cc30b3f070b14096799b8c6f2b
Romaska-uwu <149205053+Romaska-uwu@users.noreply.github.com>
2025-10-03 08:26:17 +0300

    Add files via upload

---
+++ lab_1/ConsoleApplication1/ConsoleApplication1/Auxil.cpp
@@ -0,0 +1,21 @@
+﻿#include "Auxil.h"
+#include <ctime>
+
+namespace auxil {
+    void start() {
+        srand(static_cast<unsigned>(time(nullptr)));
+    }
+
+    double dget(double rmin, double rmax) {
+        return ((double)rand() / (double)RAND_MAX) * (rmax - rmin) + rmin;
+    }
+
+    int iget(int rmin, int rmax) {
+        return static_cast<int>(dget(static_cast<double>(rmin), static_cast<double>(rmax)));
+    }
+
+    long long fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
